bd18d1d9e9ed1cc64fb568f8c3d00c162d64e35c
sagrawal <sagrawal@primesoft.net>
2023-03-16 12:39:46 +0530

    This is a new commit for what I originally planned to be amended

---
+++ src/main/java/com/primesoft/controller/DiscoveryRestController.java
@@ -0,0 +1,21 @@
+package com.primesoft.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DiscoveryRestController {
+	
+	@Autowired
+	private Environment env;
+
+	@GetMapping("/greet")
+	public String getGreetMsg() {
+		String port= env.getProperty("server.port");
+		String msg= "Good Evening..!!  (from server :: "+ port + ")";
+		
+		return msg;
+	}
+}
